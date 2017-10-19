2831de43f2c8261f8984cc155ffda2614998f157
Qudsia Ahmad <qudsiaahmad@Aimens-MacBook-Pro.local>
2017-10-18 20:38:30 -0700

    api using promise

--- weather-interface.js
+++ weather-interface.js
@@ -4,22 +4,11 @@ $(document).ready(function() {
     let city = $('#location').val();
     $('#location').val("");
 
-    let request = new XMLHttpRequest();
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=Imperial&appid=eeb72cd89e66872719c274878618bb7a`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        let response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-
-    getElements = function(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Farenheit is ${response.main.temp} degrees.`);
-    }
-  });
-});
+     $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=Imperial&appid=eeb72cd89e66872719c274878618bb7a`).then(function(response) {
+       $('.showHumidity').text(`${city} - humidity level is ${response.main.humidity}%`);
+       $('.showTemp').text(`The temperature in ${city} is ${response.main.temp} degrees.`);
+     }).fail(function(error) {
+       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+     });
+   });
+ });
