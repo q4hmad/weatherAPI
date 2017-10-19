df5ea3674f1b0ebe09d92358863538a98bac7d77
Qudsia Ahmad <qudsiaahmad@Aimens-MacBook-Pro.local>
2017-10-18 20:01:22 -0700

    Using the weather api and showing in farenheit

--- weather-interface.js
+++ weather-interface.js
@@ -1,24 +1,25 @@
-var apiKey = require('./../.env').apiKey;
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
+
     let request = new XMLHttpRequest();
-   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=Imperial&appid=eeb72cd89e66872719c274878618bb7a`;
 
-   request.onreadystatechange = function() {
-     if (this.readyState === 4 && this.status === 200) {
-       let response = JSON.parse(this.responseText);
-       getElements(response);
-     }
-   }
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
 
-   request.open("GET", url, true);
-   request.send();
+    request.open("GET", url, true);
+    request.send();
 
-   getElements = function(response) {
-     $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-   }
- });
+    getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Farenheit is ${response.main.temp} degrees.`);
+    }
+  });
 });
